749c6889d1c85d7e82596a2acc8c282c6d8b7385
Pedro De Campos Faria <pedroafonso939@yahoo.com.br>
2022-08-25 18:29:31 -0300

    Projeto Spring Boot

---
+++ src/main/java/com/lucasangelo/todosimple/TodosimpleApplication.java
@@ -0,0 +1,14 @@
+package com.lucasangelo.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosimpleApplication.class, args);
+		
+	}
+
+}
